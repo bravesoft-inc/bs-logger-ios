86c03febef89b300b88b5cde2ad079928c530300
h-taro <t.hiraishi@bravesoft.co.jp>
2022-09-22 11:27:43 +0900

    アクセス制御 更新

--- Sources/BSLogger/BSDeinitLogger.swift
+++ Sources/BSLogger/BSDeinitLogger.swift
@@ -5,7 +5,7 @@
 //  Created by 平石　太郎 on 2022/09/22.
 //
 
-public class BSDeinitLogger {
+open class BSDeinitLogger {
     deinit {
         if BSLogger.environment == .develop {
             let className = BSLogger.getClassName(from: #file)
